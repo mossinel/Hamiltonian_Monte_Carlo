35e7e24069e9a6302d427cb26f3e9318b5951795
maxencehof <maxence.hofer@epfl.ch>
2022-12-21 12:08:12 +0100

    Update d.py

--- src/d.py
+++ src/d.py
@@ -19,24 +19,23 @@ def dU(q, alpha):
 
 def Verlet(q0, p0, eps, alpha, m):
     p_tmp = p0 - eps/2*dU(q0, alpha)
-    q = q0+eps*np.divide(p_tmp, m)
+    q = q0 + eps*np.divide(p_tmp, m)
     p = p_tmp - eps/2*dU(q, alpha)
     return q, p
 
-def Metropolis_Hastings(f, alpha, eps, m1, m2, T):
-    pass
+
 
 def Hamiltonian_Monte_Carlo(q0, m, N, eps, alpha):
     q = np.zeros([N+1, 2])
-    q[0]=q0
+    q[0, :]=q0
     for i in range(N):
         p = st.norm.rvs(loc=0, scale=m)
-        q_star, p_star = Verlet(q[i], p, eps, alpha, m)
+        q_star, p_star = Verlet(q[i, :], p, eps, alpha, m)
         u = np.random.uniform()
-        if (u<np.exp(-U(q_star, alpha)+U(q[i], alpha)-K(p_star, m)+K(p, m))):
-            q[i+1] = q_star
+        if (u<np.exp(-U(q_star, alpha)+U(q[i, :], alpha)-K(p_star, m)+K(p, m))):
+            q[i+1, :] = q_star
         else:
-            q[i+1] = q[i]
+            q[i+1, :] = q[i, :]
     
     return q
 
@@ -47,23 +46,45 @@ def Hamiltonian_Monte_Carlo(q0, m, N, eps, alpha):
 def main():
     q0 = [0, 0]
     eps = 0.01
-    alpha = 10**3
+    alpha = 10**1
     m = [1, 1]
-    T = 100
+    T = 40
     N = int(np.floor(T/eps))
 
-    q = Hamiltonian_Monte_Carlo(q0, m, N, eps, alpha)
+    n = 3000
+    final_q = np.zeros([n, 2])
+
+    #final_q[:, :] = Hamiltonian_Monte_Carlo(q0, m, N, eps, alpha)[-1, :]
+    #print(np.shape(final_q))
+    
+    for i in range(n):
+        q = Hamiltonian_Monte_Carlo(q0, m, N, eps, alpha)
+        final_q[i, :] = q[-1, :]
+        if (i+1)%100 == 0:
+            print("Iteration: ", i+1, "/", n)
+    
+    #q = Hamiltonian_Monte_Carlo(q0, m, N, eps, alpha)
 
     t = np.linspace(0, T, N+1)
+    v = np.exp(-alpha*(q[:, 0]**2+q[:, 1]**2-1/4)**2)
+    x = np.linspace(-1, 1, 100)
+    y = np.zeros([100, 100])
+    for i in range(100):
+        y[:, i] = np.exp(-alpha*(x[:]**2+x[i]**2-1/4)**2)
 
-    plt.plot(t, q[:, 1])
+    fig, axs = plt.subplots(2, 2) 
+    axs[0, 0].hist(final_q[:, 0], 20, density=False)
+    axs[0, 1].hist(final_q[:, 1], 20, density=False)
+    axs[1, 0].hist2d(final_q[:, 0], final_q[:, 1], bins=(20, 20), cmap=plt.cm.jet)
+    axs[1, 1].pcolormesh(x, x, y, cmap=plt.cm.jet)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(t, q[:, 0])
+    ax2.plot(t, q[:, 1])
 
     plt.show()
 
     
-
-
-
 
 
 
